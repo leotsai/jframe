1aefea226a05216ca94e4c167d9dfbd7bba3c7af
leo <leo.tsai.2010@gmail.com>
2017-05-25 21:48:26 +0800

    added more files missing

---
+++ 99.jframe-web-main/src/main/webapp/_js-source/_core/app/21.weixin.js
@@ -0,0 +1,17 @@
+﻿(function() {
+    mvcApp.weixin = {
+        showShareTip: function(message) {
+            if (message == undefined) {
+                message = "请点击右上角“...”，然后选择分享到朋友圈";
+            }
+            var html = '<div id="shareTip">\
+                            <div class="message">' + message + '</div>\
+                            <div class="btn-close">轻按退出</div>\
+                        </div>';
+            $("#body").append(html);
+            $("#shareTip").click(function() {
+                $(this).remove();
+            });
+        }
+    };
+})();
---
+++ 99.jframe-web-main/src/main/webapp/_js-source/pc/home/index/page.js
@@ -0,0 +1,8 @@
+/**
+ * Created by Leo on 2017/1/9.
+ */
+(function(){
+    $(document).ready(function(){
+       $("#navHome").addClass("selected");
+    });
+})();
