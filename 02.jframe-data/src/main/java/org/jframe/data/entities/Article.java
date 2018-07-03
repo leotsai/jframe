d31f558b5b718e7795edca30dcf60c6d9929bee3
yezi <335376584@qq.com>
2018-07-03 13:33:10 +0800

    article entity

---
+++ 02.jframe-data/src/main/java/org/jframe/data/entities/Article.java
@@ -0,0 +1,79 @@
+package org.jframe.data.entities;
+
+import javax.persistence.Column;
+import javax.persistence.Entity;
+import javax.persistence.Table;
+
+/**
+ * created by yezi on 2018/7/2
+ */
+@Entity
+@Table(name = "s_articles")
+public class Article {
+
+    @Column(name = "title", columnDefinition = "varchar(255) not null COMMENT '文章标题'")
+    private String title;
+
+    @Column(name = "keywords", columnDefinition = "longtext null COMMENT '关键字'")
+    private String keywords;
+
+    @Column(name = "url_key", columnDefinition = "varchar(255) not null COMMENT 'URL标识'")
+    private String urlKey;
+
+    @Column(name = "description", columnDefinition = "longtext null COMMENT '描述'")
+    private String description;
+
+    @Column(name = "reads", columnDefinition = "int not null COMMENT '阅读量'")
+    private int reads = 0;
+
+    @Column(name = "author", columnDefinition = "longtext null COMMENT '作者'")
+    private String author;
+
+    public String getTitle() {
+        return title;
+    }
+
+    public void setTitle(String title) {
+        this.title = title;
+    }
+
+    public String getKeywords() {
+        return keywords;
+    }
+
+    public void setKeywords(String keywords) {
+        this.keywords = keywords;
+    }
+
+    public String getUrlKey() {
+        return urlKey;
+    }
+
+    public void setUrlKey(String urlKey) {
+        this.urlKey = urlKey;
+    }
+
+    public String getDescription() {
+        return description;
+    }
+
+    public void setDescription(String description) {
+        this.description = description;
+    }
+
+    public int getReads() {
+        return reads;
+    }
+
+    public void setReads(int reads) {
+        this.reads = reads;
+    }
+
+    public String getAuthor() {
+        return author;
+    }
+
+    public void setAuthor(String author) {
+        this.author = author;
+    }
+}
