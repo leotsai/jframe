103182ce995bbd69c6afd1a2dd16a8be427597a3
qq <867919505@qq.com>
2018-08-13 17:21:17 +0800

    refactor XssFilter

--- 50.jframe-web/src/main/java/org/jframe/web/security/XssFilter.java
+++ 50.jframe-web/src/main/java/org/jframe/web/security/XssFilter.java
@@ -2,18 +2,20 @@ package org.jframe.web.security;
 
 import org.jframe.infrastructure.AppContext;
 import org.springframework.web.method.HandlerMethod;
-import org.springframework.web.servlet.HandlerExecutionChain;
 import org.springframework.web.servlet.mvc.method.annotation.RequestMappingHandlerMapping;
 
 import javax.servlet.*;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.regex.Pattern;
 
 /**
  * @author qq
  * @date 2018/7/16
  */
 public class XssFilter implements Filter {
+    private static Pattern patterns = Pattern.compile(".(css|js|png|jpg|jpeg)$");
+
     @Override
     public void init(FilterConfig filterConfig) throws ServletException {
 
@@ -22,22 +24,23 @@ public class XssFilter implements Filter {
     @Override
     public void doFilter(ServletRequest request, ServletResponse response, FilterChain chain) throws IOException, ServletException {
         HttpServletRequest req = (HttpServletRequest) request;
-        RequestMappingHandlerMapping handlerMapping = AppContext.getBean(RequestMappingHandlerMapping.class);
-        HandlerMethod handlerMethod;
-        try {
-            handlerMethod = (HandlerMethod) handlerMapping.getHandler(req).getHandler();
-        } catch (Exception e) {
-            handlerMethod = null;
-            chain.doFilter(new XssHttpServletRequestWrapper(req), response);
+        if (patterns.matcher(req.getRequestURL().toString()).find()) {
+            chain.doFilter(request, response);
+            return;
         }
-        if (handlerMethod != null) {
+        RequestMappingHandlerMapping handlerMapping = AppContext.getBean(RequestMappingHandlerMapping.class);
+        try {
+            HandlerMethod handlerMethod = (HandlerMethod) handlerMapping.getHandler(req).getHandler();
             IgnoreXssFilter ignoreXssFilter = handlerMethod.getMethodAnnotation(IgnoreXssFilter.class);
             if (ignoreXssFilter == null || !ignoreXssFilter.value()) {
                 chain.doFilter(new XssHttpServletRequestWrapper(req), response);
             } else {
                 chain.doFilter(request, response);
             }
+        } catch (Exception e) {
+            chain.doFilter(request, response);
         }
+
     }
 
     @Override
